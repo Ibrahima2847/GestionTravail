dfe0bda421a67dc53c3b4dd6745845fb5524e07e
ibrahima2847 <ibuseck670@gmail.com>
2023-01-01 23:41:54 +0000

    23:41

---
+++ app/Http/Controllers/OuvrierController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ouvrier;
+use Illuminate\Http\Request;
+
+class OuvrierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('Admin.ouvrier');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ouvrier  $ouvrier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ouvrier $ouvrier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ouvrier  $ouvrier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ouvrier $ouvrier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ouvrier  $ouvrier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ouvrier $ouvrier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ouvrier  $ouvrier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ouvrier $ouvrier)
+    {
+        //
+    }
+}
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{config('app.name')}} - @yield('title')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -12,15 +12,15 @@
         <script
         src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"defe></script>
 
-      <script src="./assets/js/init-alpine.js"></script>
       {{-- <link rel="stylesheet"href="{{asset('assets3/lib/bootstrap/css/bootstrap.css')}}"> --}}
 
     </head>
     <body class="antialiased">
+
         @include('navbar/navbar')
-        {{--toues les seront afficbhees ici--}}
-       @yield('content')
-       <script>"{{asset('assets3/lib/bootstrap/js/bootstrap.js')}}"</script>
+
+        @yield('content')
+
     </body>
 
 </html>
